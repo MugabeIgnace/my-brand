34b7d4f81718206819960ca73d8500c290483597
Mugabe Ignace <ignacemugabe@gmail.com>
2022-12-16 06:52:26 +0200

    validate user account using local storage

--- UI/js/signup.js
+++ UI/js/signup.js
@@ -1,27 +1,60 @@
 
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
+let form = document.getElementById('form');
+let username = document.getElementById('username');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+
+
+function addAccount() {
+    var Name = username.value;
+    var Email = email.value;
+    var Password = password.value;
+
+    var user = {
+        username: Name,
+        email: Email,
+        password: Password
+    };;
+    let accounts = JSON.parse(localStorage.getItem('accounts')) || [];
+    let found = accounts.length && JSON.parse(localStorage.getItem('accounts')).some(data => data.Name.toLowerCase() == username.value.toLowerCase() && data.Email.toLowerCase() == email.value.toLowerCase());
+    if (!found) {
+        localStorage.setItem('user', JSON.stringify(user));
+        accounts.push(user);
+
+
+        //const storeduser = JSON.parse(localStorage.getItem('user'));
+
+    }
+    else {
+        alert("The Account already exists");
+    }
+
+
+
+    // var newUser = window.localStorage.getItem("user");
+    // console.log(JSON.stringify(user));
+
+}
+
 
 //  Error message
 function showError(input, message) {
-    const formControl = input.parentElement;
+    let formControl = input.parentElement;
     formControl.className = 'control error';
-    const small = formControl.querySelector('small');
+    let small = formControl.querySelector('small');
     small.innerText = message;
 }
 
 // success 
 
 function showSuccess(input) {
-    const formControl = input.parentElement;
+    let formControl = input.parentElement;
     formControl.className = 'control success';
 }
 
 // Check email 
 function checkEmail(input) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(input.value.trim())) {
         showSuccess(input);
     } else {
@@ -71,6 +104,8 @@ function passwordLength(password, min, max) {
         );
     } else {
         showSuccess(password);
+
+        alert("Account created successfully");
         window.location.href = "./login.html";
     }
 }
@@ -86,9 +121,12 @@ function getFieldName(input) {
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
+
+
     checkRequired([username, email, password]);
     checkLength(username, 3, 20);
     checkEmail(email);
     passwordLength(password, 6, 12);
-});
+    addAccount();
 
+});
